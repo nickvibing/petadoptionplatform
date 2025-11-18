b3969f97237428309dd2970de74ac086136e71fc
nickvibing <nicklauatan@gmail.com>
2025-11-18 15:36:16 -0800

    Remove duplicate client directory

--- client/js/userRegistration.js
+++
@@ -1,7 +0,0 @@
-const role = document.getElementById('role');
-const providerFields = document.getElementById('providerFields');
-
-role.addEventListener('change', () => {
-    providerFields.style.display = (role.value === 'provider') ? 'block' : 'none';
-});
-providerFields.style.display = (role.value === 'provider') ? 'block' : 'none';
